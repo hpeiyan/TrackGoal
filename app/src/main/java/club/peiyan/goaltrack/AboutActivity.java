290c7b7bba9ad187ebf10bdda942d86f7e437485
黄佩雁 <huangpeiyan@zuzuche.com>
2018-08-04 23:18:20 +0800

    修复Bug。

--- app/src/main/java/club/peiyan/goaltrack/AboutActivity.java
+++ app/src/main/java/club/peiyan/goaltrack/AboutActivity.java
@@ -1,12 +1,30 @@
 package club.peiyan.goaltrack;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.alipay.sdk.app.EnvUtils;
+import com.alipay.sdk.app.PayTask;
 import com.umeng.analytics.MobclickAgent;
 
+import java.util.Map;
+
 import butterknife.ButterKnife;
+import club.peiyan.goaltrack.pay.OrderInfoUtil2_0;
+import club.peiyan.goaltrack.pay.PayResult;
+
+import static club.peiyan.goaltrack.data.Constants.APPID;
+import static club.peiyan.goaltrack.data.Constants.RSA2_PRIVATE;
+import static club.peiyan.goaltrack.data.Constants.RSA_PRIVATE;
 
 /**
  * Created by HPY.
@@ -16,6 +34,36 @@ import butterknife.ButterKnife;
 
 public class AboutActivity extends BaseActivity {
 
+    private static final int SDK_PAY_FLAG = 1;
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SDK_PAY_FLAG: {
+                    @SuppressWarnings("unchecked")
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        Toast.makeText(AboutActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        Toast.makeText(AboutActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+    };
 
     public static void startAboutActivity(MainActivity mMainActivity) {
         Intent mIntent = new Intent(mMainActivity, AboutActivity.class);
@@ -24,6 +72,7 @@ public class AboutActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        EnvUtils.setEnv(EnvUtils.EnvEnum.SANDBOX);//测试下沙箱模拟
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_layout);
         ButterKnife.bind(this);
@@ -42,4 +91,49 @@ public class AboutActivity extends BaseActivity {
         super.onPause();
         MobclickAgent.onPause(this);
     }
+
+    /**
+     * 支付宝支付业务
+     *
+     * @param v
+     */
+    public void payV2(View v) {
+        if (TextUtils.isEmpty(APPID) || (TextUtils.isEmpty(RSA2_PRIVATE) && TextUtils.isEmpty(RSA_PRIVATE))) {
+            new AlertDialog.Builder(this).setTitle("警告").setMessage("需要配置APPID | RSA_PRIVATE")
+                    .setPositiveButton("确定", (dialoginterface, i) -> {
+                        finish();
+                    }).show();
+            return;
+        }
+
+        /**
+         * 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
+         * 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
+         * 防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
+         *
+         * orderInfo的获取必须来自服务端；
+         */
+        boolean rsa2 = (RSA2_PRIVATE.length() > 0);
+        Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(APPID, rsa2);
+        String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
+
+        String privateKey = rsa2 ? RSA2_PRIVATE : RSA_PRIVATE;
+        String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
+        final String orderInfo = orderParam + "&" + sign;
+
+        Runnable payRunnable = () -> {
+            PayTask alipay = new PayTask(AboutActivity.this);
+            Map<String, String> result = alipay.payV2(orderInfo, true);
+            Log.i("msp", result.toString());
+
+            Message msg = new Message();
+            msg.what = SDK_PAY_FLAG;
+            msg.obj = result;
+            mHandler.sendMessage(msg);
+        };
+
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+
 }
--- app/src/main/java/club/peiyan/goaltrack/data/Constants.java
+++ app/src/main/java/club/peiyan/goaltrack/data/Constants.java
@@ -17,6 +17,35 @@ public class Constants {
     /*Setting Config */
     public static final String SCORE_SHOW_DAY = "SCORE_SHOW_DAY";
 
+    /** 支付宝支付业务：入参app_id */
+    public static final String APPID = "2018080460967172";
+    public static final String RSA2_PRIVATE = "MIIEogIBAAKCAQEAuum84dEwKg/2gUjtUzfhEI/3p+lmYvvCDg7ztDIr3R8JToi8\n" +
+            "W3Nv/cf582DtYtsEt96HzvM6Sw2QR/UYIY3GfC9sl4nEtIZRiA6bW6KeE3SG3HTH\n" +
+            "JkSQGAMZw/TUCH2SUUPI/NAIYgqPbiT5wWjaCpbYvwIYmsvIAk/+Pm6W3Dfj5vXh\n" +
+            "gKCAHo/2NkAXnzX7baOgu4sm6DSrKLNplVPXgZnIBgAP0iV83ycUg3/NOQ6xftcI\n" +
+            "gpHvyHwmTsfzemBjNlASaNI3C65L7wIP6Fee3v+5MLV7+7XLX54HryGBkoYHWMwY\n" +
+            "LxOa5KdqK7OpizTyMRRhB5v0X6ZynsllmpFfzwIDAQABAoIBAE8WK3eZHboOew0F\n" +
+            "IHX/xNlPMUj49vuW7usn5HIwcGLliPiT3q8LTy2geeBC+O7FIV/zZp9oRxgOqyfn\n" +
+            "SsiXxo4xOs972aFQ3Bqno5DIY/5zZcTOHJ3SkO24EiTZ6QWWrFx6RAbEt2dUdI/0\n" +
+            "UUFG7ktgvGFFr8d/xidEVZ1xCaAvw590CyJr1gwbNZYyjqzP7jl4mGKgVFB1+C1u\n" +
+            "AbCGPXlHbSqejLDVkLSMXMB328R5fYePqeG8hv3IpKlrf2VhEwLDK15U9eiLKl1z\n" +
+            "AXM8rchcQRbg6xgo04gt/SVLx2D9tzZ9X7uzRceD/PuLch9hWkr0JKNKDp2gWyiN\n" +
+            "StkRdMECgYEA9Do9KvQIBdVsPNqaraZxfGlKhTlDq1vvPkXcOB67csxVns0oKzHN\n" +
+            "PqrTXpUnwdctUkh4hJUnfOrVN0pxklVeUqswbRnoLOZnvDWxQODjemuHtCPI6Mxt\n" +
+            "LYUCGy0WpehqJNLG847hxRzvFvmL9iEdk4n/Q4NRmZ1uVG1sbbwYo1ECgYEAw+w9\n" +
+            "bNZsWFAbdIMaFBDR+8zzARN0Ai2pCArnvrAJZEA8Ed1ukE7cnSmW9k516qlXijkl\n" +
+            "bjg1brohlZPycjJtR7I6ar7uSLlO4VP5UbHkNT5UbTsjirMrx3GJSB0ADO/pCKY3\n" +
+            "KMnlH3rucApmDlc+oG+rMUc5cU5LdfFToZDryR8CgYAmyETEWGIZww1gEHq1jpPA\n" +
+            "5Ntvkmtbf8rbAa75vm+XyDjkccGqMktcYzYTc+4PSwMoaNeyfhj6LjRThl/IlAPZ\n" +
+            "btPOz2leD+xzJ7eRDOUyoHa5NWGHDkdcDh4KgLty9xg75rivHoZdjb2t0UzXdeTD\n" +
+            "dAt0BhtkIGrPv1yThGYJMQKBgH7ESCo3wvqROnJ0sVbyWyt1PHYhxBn0uT2+8Jtt\n" +
+            "A2aeOT/F6lfs7smxsz/DHICs+TsyJoneTJRMUpaZl3FUN2kAZNPUnhDn9aXN06+M\n" +
+            "ToA0yCO09BY2P8lYKN9EBFhakolKIr59g4aO3+AWmloFETx1hGrqDrzFUAYME3Si\n" +
+            "dC05AoGAXkHPJOexsk3NfR2FXmNRg6M6AtpLhbSuflr4T3qpORWD8QVc2sBtRGTR\n" +
+            "k/KErM+5qCTIK2gJjZrWksF/FbRxo5ROD2G5zy99/ozDDfPXRfJTnJTro4oHoMHl\n" +
+            "lQX2KMscaP7HaH+CnQER4b6ZAdINeNe0NiXTshfUjQYD1Jyfdvo=";
+    public static final String RSA_PRIVATE = "";
+
     /**
      * 分数显示的天数
      *
